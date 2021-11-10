576b49ffbf4122c161ede64de09e571f5eda34ac
Denis Borodin <denis.ig.borodin001@gmail.com>
2021-11-10 23:11:32 +0100

    Added button commands for the player

--- Project Mafia/Assets/Scripts/WeaponStats.cs
+++ Project Mafia/Assets/Scripts/WeaponStats.cs
@@ -6,4 +6,22 @@ public class WeaponStats : MonoBehaviour
     public float fireRate;
     public int health;
     public bool isFood;
+
+    public GameObject button;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            button.SetActive(false);
+        }
+    }
 }
